79225f64aa309ce66c9628f096db2577c0d16c63
kimmh74 <zziipp.mw@gmail.com>
2025-10-29 11:43:30 +0900

    f3/aop

---
+++ src/main/java/com/kmh/ex25_branch/aspect/ExceptionAspect.java
@@ -0,0 +1,20 @@
+package com.kmh.ex25_branch.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class ExceptionAspect {
+
+	@AfterThrowing(pointcut = "execution(* com.kmh.ex25_branch.service.*.*(..))", throwing = "excep")
+	public void logError(JoinPoint jp, Exception excep) {
+		log.info("ererererer 메서중 오류 발생: {} erer \n ererer {} ererer", jp.getSignature(), excep.getMessage());
+
+	}
+}
