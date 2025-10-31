0fa063bb8c2c3bcfc7c19cbb95cfaece0d2aff89
kimmh74 <zziipp.mw@gmail.com>
2025-10-31 16:11:00 +0900

    restcontroller_완성

--- src/main/java/com/kmh/ex25_branch/controller/StudentRestController.java
+++ src/main/java/com/kmh/ex25_branch/controller/StudentRestController.java
@@ -2,7 +2,13 @@ package com.kmh.ex25_branch.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +18,55 @@ import com.kmh.ex25_branch.service.StudentService;
 import lombok.RequiredArgsConstructor;
 
 
+
+
+
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
 public class StudentRestController {
 	
 	private final StudentService studentService;
+	
+
+	
+//	#################### 여기부터 #################################
+//	전체 조회: GET http://localhost:8080/api/students	
+	@GetMapping
+	public List<Student> list() {
+		return studentService.getAllStudents();
+	}
+//	단건 조회 : get http://localhost:8080/api/student/id => 요청경로는 getMapping밖에 못쓴다.
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> detail(@PathVariable Long id) {
+		Student student = studentService.getStudent(id);
+		return ResponseEntity.ok(student);
+	}
+//	등록 : POST http://localhost:8080/api/students 포스트맨 POSTMAN 으로 확인
+	@PostMapping
+	public ResponseEntity<Student> create(@RequestBody Student student) {
+		studentService.createStudent(student);		
+		return ResponseEntity.ok(student);
+	}
+//	수정 put작업
+	@PutMapping("/{id}")
+	public ResponseEntity<Student> update(@PathVariable Long id, @RequestBody Student student) {
+		student.setId(id);
+		studentService.updeteStudent(student);
+		return ResponseEntity.ok(student);
+	}
+//	삭제 작업 DELETE
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		studentService.deleteStudent(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	
+	
+	
+	
+	
 	
 //	@GetMapping
 //	public String list() {
@@ -34,14 +83,6 @@ public class StudentRestController {
 //		return studentService.getStudent();
 //	}
 
-	
-//	#################### 여기부터 #################################
-//	전체 조회: GET http://localhost:8080/api/students	
-	@GetMapping
-	public List<Student> list() {
-		return studentService.getAllStudents();
-	}
-	
 	
 
 }
--- src/main/java/com/kmh/ex25_branch/service/StudentService.java
+++ src/main/java/com/kmh/ex25_branch/service/StudentService.java
@@ -15,55 +15,57 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class StudentService {
 
-//	public String getMyString() {
-//		
-//		return "test 테스트";
-//	}
-//
-//	public Integer getMyInteger() {
-//		return 99999;
-//	}
-//
-//	public MyStudent getStudent() {
-//// java방식
-//		MyStudent myStudent = new MyStudent();
-//		myStudent.setName("홍길동");
-//		myStudent.setAge(10);		
-//		return myStudent;
-//	}
-//
-//	public String getStudentName() {
-//		MyStudent myStudent2 = new MyStudent();
-//		myStudent2.setName("일지매222");
-//		myStudent2.setAge(20);
-//		return myStudent2.getName();
-//	}
-	
 	private final StudentMapper studentMapper;
-	
-	public List<Student> getAllStudents(){
+
+	public List<Student> getAllStudents() {
+//		매퍼 오브젝트에게 값을 요청
 		return studentMapper.findAll();
 	}
-	
+
 	public Student getStudent(Long id) {
+//		
 		return studentMapper.findById(id);
 	}
-	
+
 	@Transactional
 	public void createStudent(Student student) {
 		studentMapper.insert(student);
 	}
-	
+
 	@Transactional
 	public void updeteStudent(Student student) {
 		studentMapper.update(student);
 	}
-	
+
 	@Transactional
-	public void deleteStudent(Long id ) {
+	public void deleteStudent(Long id) {
 		studentMapper.delete(id);
 	}
-
-
 	
+	
+	
+//	public String getMyString() {
+//	
+//	return "test 테스트";
+//}
+//
+//public Integer getMyInteger() {
+//	return 99999;
+//}
+//
+//public MyStudent getStudent() {
+////java방식
+//	MyStudent myStudent = new MyStudent();
+//	myStudent.setName("홍길동");
+//	myStudent.setAge(10);		
+//	return myStudent;
+//}
+//
+//public String getStudentName() {
+//	MyStudent myStudent2 = new MyStudent();
+//	myStudent2.setName("일지매222");
+//	myStudent2.setAge(20);
+//	return myStudent2.getName();
+//}
+
 }
